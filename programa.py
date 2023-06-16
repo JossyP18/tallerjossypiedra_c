95ffc715a94e8482c7e369ddd6fe9d95557de65b
JossyP18 <136769527+JossyP18@users.noreply.github.com>
2023-06-16 01:56:50 +0000

    jossy

---
+++ programa.py
@@ -0,0 +1,9 @@
+from matematicas import sumar, restar, multiplicar, dividir
+from geometria import distancia
+
+print(sumar(2,3))
+print (restar (4,2))
+print (multiplicar (4,5))
+print(dividir (2,6))
+
+print ("Distancia:", distancia (1,2,4,5))
